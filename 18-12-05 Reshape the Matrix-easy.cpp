9f1008bb3eac115c9fc696b0ea59d23d69a4fa37
enderson1941 <enderson1941@gmail.com>
2018-12-05 14:36:54 +0900

    1436

---
+++ 18-12-05 Reshape the Matrix-easy.cpp
@@ -0,0 +1,69 @@
+/*
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+Example 1:
+Input:
+nums =
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output:
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+
+Example 2:
+Input:
+nums =
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output:
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+
+Note:
+The height and width of the given matrix is in range [1, 100].
+The given r and c are all positive.
+*/
+
+class Solution
+{
+public:
+  vector<vector<int>> matrixReshape(vector<vector<int>>& nums,
+    int r, int c)
+  {
+    int index = 0;
+  	int row = nums.size();
+  	int col = nums[0].size();
+  	vector<int> temp_;
+  	vector<vector<int>> answer;
+  	if (r*c != row*col)
+  	{
+  		return nums;
+  	}
+  	for (int i = 0; i < row; i++)
+  	{
+  		for (int j = 0; j < col; j++)
+  		{
+  			if (index != c)
+  			{
+  				temp_.push_back(nums[i][j]);
+  				index++;
+  			}
+  			else
+  			{
+  				answer.push_back(temp_);
+  				temp_.clear();
+  				index = 0;
+  				temp_.push_back(nums[i][j]);
+  				index++;
+  			}
+  		}
+  	}
+  	answer.push_back(temp_);
+    return answer;
+    //runtime: 40ms
+  }
+};
